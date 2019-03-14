e94caae9d88ce00013c57a0d2b2689f4ee4d560b
Jay <jonathanbazan90@gmail.com>
2019-03-14 13:33:35 -0700

    This is the new stuff haha

--- constants.py
+++ constants.py
@@ -4,4 +4,5 @@ Holds constant values used for encryption and decryption
 
 IV_LEN = 16     # length of initialization vector
 KEY_LEN = 32    # length of key
+HMAC_KEY_LEN = 32 #length of Hash Message Authentication Code
 FOLDER_PATH = "test-files/"     # path to test files (varies on computer)
