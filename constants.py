d3207126ab36bfd98e21381df594a422261256aa
Joshua Kasanjian <jkasanjian@gmail.com>
2019-03-19 12:12:44 -0700

    upate constants

--- constants.py
+++ constants.py
@@ -4,4 +4,4 @@ Holds constant values used for encryption and decryption
 
 IV_LEN = 16         # length of initialization vector
 KEY_LEN = 32        # length of key for encryption
-HMAC_KEY_LEN = 16   # length of key for HMAC tag generation key
+HMAC_KEY_LEN = 32   # length of key for HMAC tag generation key 
